7815dafb469a221fcad136063357ad89aedce5bc
김준형 <kjhjh04003@naver.com>
2021-05-14 14:56:18 +0900

    조인문제 1~4

--- JOIN_quiz.sql
+++ JOIN_quiz.sql
@@ -17,17 +17,33 @@ WHERE emp.department_id=dept.department_id AND emp.department_id IS NOT NULL ORD
 -- OR
 SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
 FROM employees emp JOIN departments dept ON emp.department_id=dept.department_id JOIN jobs j ON emp.job_id=j.job_id WHERE emp.department_id IS NOT NULL ORDER BY employee_id;
+-- OR
+SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
+FROM departments dept, employees emp, jobs j
+WHERE emp.job_id=j.job_id AND emp.department_id=dept.department_id AND emp.department_id IS NOT NULL ORDER BY employee_id;
 
 -- 2-1. 문제 2에서 부서가 없는 사번 178을 표시
--- select * from employees where department_id IS NULL;
+-- OUTER JOIN
+-- oracle : null이 포함될 수 있는 쪽에 (+) / ANSI : lef outer join or right outer join
+-- oracle
+SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
+FROM departments dept, employees emp, jobs j
+WHERE emp.job_id=j.job_id AND emp.department_id = dept.department_id (+) ORDER BY employee_id;
+-- ANSI
+SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
+FROM employees emp LEFT JOIN departments dept ON emp.department_id=dept.department_id JOIN jobs j ON emp.job_id=j.job_id;
 
 -- 3. 도시별로 위치한 부서들을 파악
 -- 도시아이디, 도시명, 부서명, 부서아이디를 도시아이디(오름차순) 정렬
 -- 부서가 없는 도시는 표시하지 않음
-SELECT loc.location_id, city, department_name, department_id FROM locations loc, departments dept WHERE loc.location_id=dept.location_id AND dept.location_id IS NOT NULL ORDER BY loc.location_id; -- 27 개행
+SELECT loc.location_id, city, department_name, department_id FROM locations loc, departments dept WHERE loc.location_id=dept.location_id; -- 27 개행
+-- OR
+SELECT loc.location_id, city, department_name, department_id FROM locations loc JOIN departments dept ON loc.location_id=dept.location_id;
 
 -- 3-1. 문제3에서 부서가 없는 도시 표시
--- 
+SELECT loc.location_id, city, department_name, department_id FROM locations loc, departments dept WHERE loc.location_id=dept.location_id(+); --43 개행
+-- OR
+SELECT loc.location_id, city, department_name, department_id FROM locations loc LEFT JOIN departments dept ON loc.location_id=dept.location_id;
 
 -- 4. 지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로 출력하되 지역이름 오름차순, 나라이름 내림차순으로 정렬
 SELECT region_name, country_name FROM regions reg JOIN countries c ON reg.region_id=c.region_id ORDER BY region_name, c.country_name DESC; -- 25 개행
