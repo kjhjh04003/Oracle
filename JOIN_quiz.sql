00e2cf68ace82d6c814df6f2fe76860009954bed
김준형 <kjhjh04003@naver.com>
2021-05-16 17:36:40 +0900

    join 문제 1~9번, 8번 제외

--- JOIN_quiz.sql
+++ JOIN_quiz.sql
@@ -6,21 +6,26 @@ SELECT *FROM locations;
 SELECT emp.employee_id, emp.first_name, emp.last_name, dept.department_name 
 FROM employees emp 
 JOIN departments dept ON emp.department_id=dept.department_id ORDER BY dept.department_name, emp.employee_id DESC; -- 106 개행
+-- OR
+SELECT emp.employee_id, emp.first_name, emp.last_name, dept.department_name
+FROM employees emp, departments dept
+WHERE emp.department_id=dept.department_id
+ORDER BY dept.department_name, emp.employee_id DESC;
 
 -- 2. 직원 테이블의 job_id는 현재의 업무 아이디
 -- 직원들의 사번, 이름, 급여, 부서명, 현재업무를 사번 오름차순으로 정렬
 -- 부서가 없는 사번 178은 표시하지 않는다.
-SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
+SELECT emp.employee_id, first_name, salary, department_name, job_title
 FROM departments dept, employees emp 
 JOIN jobs j ON emp.job_id=j.job_id 
-WHERE emp.department_id=dept.department_id AND emp.department_id IS NOT NULL ORDER BY employee_id; -- 106개행
+WHERE emp.department_id=dept.department_id ORDER BY employee_id; -- 106개행
 -- OR
 SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
-FROM employees emp JOIN departments dept ON emp.department_id=dept.department_id JOIN jobs j ON emp.job_id=j.job_id WHERE emp.department_id IS NOT NULL ORDER BY employee_id;
+FROM employees emp JOIN departments dept ON emp.department_id=dept.department_id JOIN jobs j ON emp.job_id=j.job_id ORDER BY employee_id;
 -- OR
 SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
 FROM departments dept, employees emp, jobs j
-WHERE emp.job_id=j.job_id AND emp.department_id=dept.department_id AND emp.department_id IS NOT NULL ORDER BY employee_id;
+WHERE emp.job_id=j.job_id AND emp.department_id=dept.department_id ORDER BY employee_id;
 
 -- 2-1. 문제 2에서 부서가 없는 사번 178을 표시
 -- OUTER JOIN
@@ -28,7 +33,7 @@ WHERE emp.job_id=j.job_id AND emp.department_id=dept.department_id AND emp.depar
 -- oracle
 SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
 FROM departments dept, employees emp, jobs j
-WHERE emp.job_id=j.job_id AND emp.department_id = dept.department_id (+) ORDER BY employee_id;
+WHERE emp.job_id=j.job_id AND emp.department_id = dept.department_id (+) ORDER BY employee_id; -- 107 개행
 -- ANSI
 SELECT emp.employee_id, first_name, salary, department_name, job_title, emp.department_id
 FROM employees emp LEFT JOIN departments dept ON emp.department_id=dept.department_id JOIN jobs j ON emp.job_id=j.job_id;
@@ -47,9 +52,16 @@ SELECT loc.location_id, city, department_name, department_id FROM locations loc 
 
 -- 4. 지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로 출력하되 지역이름 오름차순, 나라이름 내림차순으로 정렬
 SELECT region_name, country_name FROM regions reg JOIN countries c ON reg.region_id=c.region_id ORDER BY region_name, c.country_name DESC; -- 25 개행
+-- OR
+SELECT region_name, country_name FROM regions reg, countries c WHERE reg.region_id=c.region_id ORDER BY region_name, c.country_name DESC;
 
 -- 5. 자신의 매니저보다 채용일(hire_date)이 빠른 사원의 사번, 이름, 채용일, 매니저이름(first_name), 매니저 입사일을 조회
--- SELECT emp.employee_id, emp.first_name, emp.hire_date, man.first_name, man.hire_date FROM employees emp, employees man WHERE emp.hire_date < (SELECT hire_date FROM employees WHERE emp.employee_id=man.employee_id);
+-- 자신의 매니저 찾기
+SELECT emp.employee_id, emp.first_name, emp.hire_date, man.first_name, man.hire_date FROM employees emp, employees man
+WHERE emp.manager_id=man.employee_id AND emp.hire_date < man.hire_date; -- 37 개행
+-- OR
+SELECT emp.employee_id, emp.first_name, emp.hire_date, man.first_name, man.hire_date FROM employees emp JOIN employees man
+ON emp.manager_id=man.employee_id WHERE emp.hire_date < man.hire_date;
 
 -- 6. 나라별로 어떠한 부서들이 위치하고 있는지 파악
 -- 나라명, 나라아이디, 도시명, 도시아이디, 부서명, 부서아이디를 나라명(오름차순)으로 정렬
@@ -57,6 +69,10 @@ SELECT region_name, country_name FROM regions reg JOIN countries c ON reg.region
 SELECT c.country_name, c.country_id, loc.city, loc.location_id, dept.department_name, dept.department_id 
 FROM countries c, locations loc JOIN departments dept ON loc.location_id=dept.location_id 
 WHERE c.country_id=loc.country_id ORDER BY c.country_name; -- 27 개행
+-- OR
+SELECT c.country_name, c.country_id, loc.city, loc.location_id, dept.department_name, dept.department_id 
+FROM countries c, locations loc, departments dept
+WHERE loc.location_id=dept.location_id  AND c.country_id=loc.country_id ORDER BY c.country_name;
 
 -- 7.job_history 테이블은 과거 담당자의 데이터
 -- 과거의 업무아이디(job_id)가 'AC_ACCOUNT'로 근무한 사번의 사번, 이름(풀네임), 업무아이디, 시작일, 종료일 출력
@@ -68,3 +84,10 @@ WHERE his.job_id='AC_ACCOUNT'; -- 2 개행
 
 --9. 각 사원에 대해서 사번, 이름, 부서명, 매니저의 이름 조회
 -- 부서가 없는 직원도 표시
+SELECT emp.employee_id, emp.first_name, dept.department_name, man.first_name
+FROM employees emp, departments dept, employees man
+WHERE emp.manager_id=man.employee_id AND emp.department_id=dept.department_id(+);
+-- or
+SELECT emp.employee_id, emp.first_name, dept.department_name, man.first_name
+FROM employees man, employees emp LEFT OUTER JOIN departments dept
+ON emp.department_id=dept.department_id WHERE emp.manager_id=man.employee_id;
